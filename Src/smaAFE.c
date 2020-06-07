47c0d4d8361d828b453afeaab59aa2cb8cacb479
TopQuark12 <tat1212wong@gmail.com>
2020-06-07 18:59:43 +0800

    bang bang ok

--- Src/smaAFE.c
+++ Src/smaAFE.c
@@ -14,17 +14,23 @@
 #include "main.h"
 #include "tim.h"
 
+#define NCoef 2
+
 uint16_t adcDataRaw[2];
-const float voltageReadingOffset = -0.2694;
-const float voltageReadingGain = 4873;
-const float currentReadingOffset = -0.01656;
-const float currentReadingGain = 78919;
-const float currentSetOffset = 0.0069586;
-const float currentSetGain = 4987;
+float voltageReadingOffset = -0.24;
+float voltageReadingGain = 4959;
+float currentReadingOffset = -0.01656;
+float currentReadingGain = 78919;
+float currentSetOffset = 0.0069586;
+float currentSetGain = 4987;
+float currentSetMin = 0.2;
+float currentSetMax = 0.5;
 static volatile float currentSet = 0;
 float currentReading;
 float voltageReading;
 float resistanceReading;
+float shrinkage;
+float shrinkageSetting;
 
 void smaAfeInit(void)
 {
@@ -34,11 +40,57 @@ void smaAfeInit(void)
 
 static volatile uint32_t dacOutRaw = 0;
 
+float cap(float val, float min, float max)
+{
+    if (val > max)
+        return max;
+    if (val < min)
+        return min;
+    return val;
+}
+
+float map(float x, float in_min, float in_max, float out_min, float out_max)
+{
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+float iir(float NewSample)
+{
+    float ACoef[NCoef + 1] = {
+        0.06745527606901530200, 0.13491055213803060000, 0.06745527606901530200};
+
+    float BCoef[NCoef + 1] = {1.00000000000000000000,
+                              -1.14298050253990090000,
+                              0.41280159809618860000};
+
+    static float y[NCoef + 1];  // output samples
+    static float x[NCoef + 1];  // input samples
+    int n;
+
+    // shift the old samples
+    for (n = NCoef; n > 0; n--)
+    {
+        x[n] = x[n - 1];
+        y[n] = y[n - 1];
+    }
+
+    // Calculate the new output
+    x[0] = NewSample;
+    y[0] = ACoef[0] * x[0];
+    for (n = 1; n <= NCoef; n++)
+        y[0] += ACoef[n] * x[n] - BCoef[n] * y[n];
+
+    return y[0];
+}
+
 void smaAfeLoop(void) 
 {
     voltageReading = adcDataRaw[0] / voltageReadingGain - voltageReadingOffset;
     currentReading = adcDataRaw[1] / currentReadingGain - currentReadingOffset;
     resistanceReading = voltageReading / currentReading;
+    shrinkage = map(resistanceReading, 4.64, 4.1, 0, 1);
+    currentSet = shrinkage < shrinkageSetting ? currentSetMax : currentSetMin;
+    currentSet = cap(currentSet, currentSetMin, currentSetMax);
     dacOutRaw = (currentSet - currentSetOffset) * currentSetGain;
     HAL_DAC_SetValue(&hdac1, DAC_CHANNEL_1, DAC_ALIGN_12B_R, (uint32_t) dacOutRaw);
 }
